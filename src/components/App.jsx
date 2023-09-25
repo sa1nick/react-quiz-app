5943019130a8f690c317be1e8684d6707f85c2ef
N1khil <saininikhilnnn@gmail.com>
2023-09-25 14:39:54 +0530

    files uploaded via git

---
+++ src/components/App.jsx
@@ -0,0 +1,158 @@
+/* eslint-disable no-case-declarations */
+// import DateCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+import Question from "./Question";
+import NexButton from "./NexButton";
+import Progress from "./Progress";
+import FinishScreen from "./FinishScreen";
+import Footer from "./Footer";
+import Timer from "./Timer";
+
+const initialState = {
+  questions: [],
+  // "loading", "error", "ready", "active", "finished"
+  status: "loading",
+  index: 0,
+  answer: null,
+  points: 0,
+  highscore: 0,
+  secondsRemaining: null,
+};
+
+const QUES_PER_SECONDS = 30;
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecieved":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+
+    case "start":
+      return {
+        ...state,
+        status: "active",
+        secondsRemaining: state.questions.length * QUES_PER_SECONDS,
+      };
+
+    case "newAnswer":
+      const question = state.questions.at(state.index);
+
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+
+    case "nextQuestion":
+      return { ...state, index: state.index + 1, answer: null };
+
+    case "finish":
+      return {
+        ...state,
+        status: "finished",
+        highscore:
+          state.points > state.highscore ? state.points : state.highscore,
+      };
+
+    case "restart":
+      return {
+        ...initialState,
+        questions: state.questions,
+        state: "ready",
+      };
+
+    case "quizSeconds":
+      return {
+        ...state,
+        secondsRemaining: state.secondsRemaining - 1,
+        status: state.secondsRemaining === 0 ? "finished" : state.status,
+      };
+
+    default:
+      throw new Error("Action unknown");
+  }
+}
+
+export default function App() {
+  const [
+    { questions, status, index, answer, points, highscore, secondsRemaining },
+    dispatch,
+  ] = useReducer(reducer, initialState);
+
+  const numQuestions = questions.length;
+
+  const maxPossiblePoints = questions.reduce(
+    (prev, cur) => prev + cur.points,
+    0
+  );
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataRecieved", payload: data }))
+      .catch((err) => console.log(`Error: ${err}`));
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+      <main className="main">
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && (
+          <>
+            <Progress
+              index={index}
+              numQuestions={numQuestions}
+              answer={answer}
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
+            />
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <Footer>
+              <Timer dispatch={dispatch} secondsRemaining={secondsRemaining} />
+              <NexButton
+                dispatch={dispatch}
+                answer={answer}
+                index={index}
+                numQuestions={numQuestions}
+              />
+            </Footer>
+          </>
+        )}
+        {status === "finished" && (
+          <>
+            <FinishScreen
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
+              highscore={highscore}
+            />
+            <button
+              className="btn btn-ui"
+              onClick={() => dispatch({ type: "restart" })}
+            >
+              Restart Quiz
+            </button>
+          </>
+        )}
+      </main>
+    </div>
+  );
+}
---
+++ src/components/Progress.js
@@ -0,0 +1,21 @@
+function Progress({ numQuestions, index, points, maxPossiblePoints, answer }) {
+  return (
+    <header className="progress">
+      <progress max={numQuestions} value={index + Number(answer != null)} />
+      <p>
+        Question
+        <strong>
+          {index + 1}/{numQuestions}
+        </strong>
+      </p>
+
+      <p>
+        <strong>
+          {points}/{maxPossiblePoints}
+        </strong>
+      </p>
+    </header>
+  );
+}
+
+export default Progress;
